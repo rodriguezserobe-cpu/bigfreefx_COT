9d79e69f2dba16d71f47bcbe966283a3af5de31d
Enese Serobe <rodriguezserobe@gmail.com>
2026-07-24 18:24:09 +0200

    add code that calls api online not locally

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+export default API;
+
+/*
+import axios from "axios";
+
+const API = axios.create({
   baseURL: "http://localhost:5000/api",
 });
 
 export default API;
+*/
