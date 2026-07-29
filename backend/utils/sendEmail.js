58ed63fc5697103b9989f5a403e6c7c7fa0b940e
Enese Serobe <rodriguezserobe@gmail.com>
2026-07-29 14:30:24 +0200

    edit emails transportor

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,11 +1,10 @@
 import "../config/env.js";
 import nodemailer from "nodemailer";
 
-console.log("EMAIL_USER:", process.env.EMAIL_USER);
-console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
