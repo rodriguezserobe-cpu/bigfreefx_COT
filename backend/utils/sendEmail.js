fbf49115d7bbe2e5426140f69c6e93b9661dd86d
Enese Serobe <rodriguezserobe@gmail.com>
2026-07-30 18:18:14 +0200

    Use official Brevo SDK for email

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,23 +1,29 @@
 import "../config/env.js";
-import * as Brevo from "@getbrevo/brevo";
+import SibApiV3Sdk from "sib-api-v3-sdk";
 
-const apiInstance = new Brevo.TransactionalEmailsApi();
+const client = SibApiV3Sdk.ApiClient.instance;
 
-apiInstance.setApiKey(
-  Brevo.TransactionalEmailsApiApiKeys.apiKey,
-  process.env.BREVO_API_KEY,
-);
+const apiKey = client.authentications["api-key"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
+
+const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
 const sendEmail = async (to, subject, html) => {
-  await apiInstance.sendTransacEmail({
+  const sendSmtpEmail = {
     sender: {
-      name: "BigFree FX",
       email: process.env.EMAIL_USER,
+      name: "BigFree FX",
     },
-    to: [{ email: to }],
+    to: [
+      {
+        email: to,
+      },
+    ],
     subject,
     htmlContent: html,
-  });
+  };
+
+  await apiInstance.sendTransacEmail(sendSmtpEmail);
 };
 
 export default sendEmail;
